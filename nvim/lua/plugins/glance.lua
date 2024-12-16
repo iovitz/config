336386ec11fbce083080e3593c917f3ef88204d5
iovitz <iovitz1999@gmail.com>
2024-12-16 17:59:33 +0800

    feat: 使用全新的format函数

--- nvim/lua/plugins/glance.lua
+++ nvim/lua/plugins/glance.lua
@@ -1,8 +1,8 @@
 return {{
     "dnlhc/glance.nvim",
     config = function()
-        vim.keymap.set('n', ';d', ':Glance definitions<CR>', opts)
-        vim.keymap.set('n', ';r', ':Glance references<CR>', opts)
+        vim.keymap.set('n', '<leader>sd', ':Glance definitions<CR>', opts)
+        vim.keymap.set('n', '<leader>sr', ':Glance references<CR>', opts)
         require('glance').setup({})
     end
 }}
